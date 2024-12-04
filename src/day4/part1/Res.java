2c6940c97a9d756c7b6883b0047d14e5eb487a33
Lucas Balon <lucasbalon@gmail.com>
2024-12-04 18:37:06 +0100

    day4/part1 ????

--- src/day4/part1/Res.java
+++ src/day4/part1/Res.java
@@ -22,14 +22,53 @@ public class Res {
             i++;
         }
 
-        //oups
         for (int y = 0; y < tab.length; ++y) {
             for (int x = 0; x < tab[y].length; ++x) {
-                System.out.print(tab[y][x]);
+                if (tab[x][y] == 'X') {
+                    if (find(tab, x, y)) {
+                        res++;
+                    }
+                }
             }
             System.out.println();
         }
 
         System.out.println(res);
+    }
+
+    public static boolean find(char[][] tab, int startX, int startY) {
+        int[][] directions = {
+                {0, 1},   // Droite
+                {0, -1},  // Gauche
+                {1, 0},   // Bas
+                {-1, 0},  // Haut
+                {1, 1},   // Diagonale droite-bas
+                {1, -1},  // Diagonale gauche-bas
+                {-1, 1},  // Diagonale droite-haut
+                {-1, -1}  // Diagonale gauche-haut
+        };
+
+        int rows = tab.length;
+        int cols = tab[0].length;
+
+        int len = word.length();
+
+        for (int[] dir : directions) {
+            boolean match = true;
+            for (int k = 0; k < len; k++) {
+                int newX = startX + k * dir[1];
+                int newY = startY + k * dir[0];
+
+                // Vérification des limites
+                if (newX < 0 || newX >= cols || newY < 0 || newY >= rows || tab[newY][newX] != word.charAt(k)) {
+                    match = false;
+                    break;
+                }
+            }
+            if (match) {
+                return true;
+            }
+        }
+        return false;
     }
 }
